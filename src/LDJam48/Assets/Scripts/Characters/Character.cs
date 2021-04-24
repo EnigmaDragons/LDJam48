6f446f47b872ecd6ef67257b5703c9ff3b25de4f
ChaosTheLegend <ivan20131321@gmail.com>
2021-04-24 11:25:48 +0600

    Added wrong answer penalty. Closes #27

--- src/LDJam48/Assets/Scripts/Characters/Character.cs
+++ src/LDJam48/Assets/Scripts/Characters/Character.cs
@@ -1,3 +1,4 @@
+using Tags;
 using UnityEngine;
 
 [CreateAssetMenu]
@@ -5,4 +6,17 @@ public class Character : ScriptableObject
 {
     public string CharacterName;
     public GameObject Prefab;
+    public TagObject[] learnedTags;
+    [SerializeField] private int suspicion;
+
+    public void AddSuspicion(int amount)
+    {
+        suspicion += amount;
+    }
+
+    public int GetSuspicion()
+    {
+        return suspicion;
+    }
+    
 }
