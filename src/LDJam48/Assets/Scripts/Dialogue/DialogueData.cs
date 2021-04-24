9042b64a8dee1aa5fba1a8ce9c53eb21d25b78a7
SilasReinagel <silas.reinagel@gmail.com>
2021-04-23 20:46:02 -0700

    Publishes Show Statement Message in Conversation. Closes #45

--- src/LDJam48/Assets/Scripts/Dialogue/DialogueData.cs
+++ src/LDJam48/Assets/Scripts/Dialogue/DialogueData.cs
@@ -7,4 +7,10 @@ public class DialogueData
     [SerializeField] private Character speakingCharacter;
     [SerializeField] private string statement;
     [SerializeField] private DialogueOption[] options;
+
+    public void Perform()
+    {
+        if (!string.IsNullOrWhiteSpace(statement))
+            Message.Publish(new ShowStatement(speakingCharacter, statement));
+    }
 }
