24a82b72483367a0ab8a2b75f04f487ec39307e3
SilasReinagel <silas.reinagel@gmail.com>
2021-04-24 22:18:37 -0700

    Conversation: Can set Emotion on NPC Statements. Closes #170

--- src/LDJam48/Assets/Scripts/Dialogue/FollowupDialogueData.cs
+++ src/LDJam48/Assets/Scripts/Dialogue/FollowupDialogueData.cs
@@ -6,6 +6,7 @@ public class FollowupDialogueData
 {
     [SerializeField] private Character speakingCharacter;
     [SerializeField] private string statement;
+    [SerializeField] private StringReference statementEmotion = new StringReference("Default"); 
     [SerializeField] private bool onlyShowIfCharacterLikedAnswer;
 
     private bool MatchesCharacter(Character c) => speakingCharacter == c;
@@ -14,5 +15,5 @@ public class FollowupDialogueData
         MatchesCharacter(c)
         && (onlyShowIfCharacterLikedAnswer && susAmount < 1) 
         || (!onlyShowIfCharacterLikedAnswer && susAmount > 0);
-    public void Begin() => Message.Publish(new ShowStatement(speakingCharacter, statement));
+    public void Begin() => Message.Publish(new ShowStatement(speakingCharacter, statement, statementEmotion));
 }
--- src/LDJam48/Assets/Scripts/Dialogue/Messages/ShowStatement.cs
+++ src/LDJam48/Assets/Scripts/Dialogue/Messages/ShowStatement.cs
@@ -3,10 +3,12 @@ public class ShowStatement
 {
     public Character SpeakingCharacter { get; }
     public string Statement { get; }
+    public StringReference Emotion { get; }
 
-    public ShowStatement(Character c, string statement)
+    public ShowStatement(Character c, string statement, StringReference emotion)
     {
         SpeakingCharacter = c;
         Statement = statement;
+        Emotion = emotion;
     }
 }
