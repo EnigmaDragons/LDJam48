5517774ec014980ce54ae500e9c32f609dec5b3d
SilasReinagel <silas.reinagel@gmail.com>
2021-04-25 18:46:30 -0700

    UI Kit 2: Location Name. Closes #282

--- src/LDJam48/Assets/Scripts/Locations/Location.cs
+++ src/LDJam48/Assets/Scripts/Locations/Location.cs
@@ -4,10 +4,12 @@ using UnityEngine.Events;
 [CreateAssetMenu]
 public class Location : ScriptableObject
 {
+    [SerializeField] private Sprite locationIcon;
     [SerializeField] private GameObject obj;
     [SerializeField] private Conversation[] conversations;
     [SerializeField] private UnityEvent onFinished;
 
+    public Sprite LocationIcon => locationIcon;
     public GameObject Obj => obj;
     public Conversation[] Conversations => conversations;
     public UnityEvent OnFinished => onFinished;
