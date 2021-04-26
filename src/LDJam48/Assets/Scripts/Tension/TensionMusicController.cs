ab1d6b97e2bdc202f172254337315aaef0a8d573
SilasReinagel <silas.reinagel@gmail.com>
2021-04-26 09:40:47 -0700

    Music: Integrated Secret Lair music. Closes #331

--- src/LDJam48/Assets/Scripts/Tension/TensionMusicController.cs
+++ src/LDJam48/Assets/Scripts/Tension/TensionMusicController.cs
@@ -11,18 +11,32 @@ public class TensionMusicController : OnMessage<GameStateChanged>
     [SerializeField] private IntroloopAudio tenseMusic;
     [SerializeField] private IntroloopAudio superTenseMusic;
 
-    private readonly Dictionary<TensionLevel, IntroloopAudio> _musics = new Dictionary<TensionLevel, IntroloopAudio>();
+    private readonly Dictionary<TensionLevel, IntroloopAudio> _defaultMusics = new Dictionary<TensionLevel, IntroloopAudio>();
+    private Dictionary<TensionLevel, IntroloopAudio> _locationMusics = new Dictionary<TensionLevel, IntroloopAudio>();
     
     private void Start()
     {
-        _musics[TensionLevel.Calm] = calmMusic;
-        _musics[TensionLevel.Tense] = tenseMusic;
-        _musics[TensionLevel.SuperTense] = superTenseMusic;
-        this.ExecuteAfterTinyDelay(() => player.PlaySelectedMusicLooping(_musics[gameState.TensionLevel]));
+        _defaultMusics[TensionLevel.Calm] = calmMusic;
+        _defaultMusics[TensionLevel.Tense] = tenseMusic;
+        _defaultMusics[TensionLevel.SuperTense] = superTenseMusic;
+        _locationMusics = gameState.CurrentLocation.Music;
+        this.ExecuteAfterTinyDelay(() => player.PlaySelectedMusicLooping(GetCurrentMusic(gameState.TensionLevel)));
     }
 
     protected override void Execute(GameStateChanged msg)
     {
-        player.CrossfadeToMusic(_musics[msg.State.TensionLevel], 0.5f, player.CurrentClipPoint);
+        player.CrossfadeToMusic(GetCurrentMusic(msg.State.TensionLevel), 0.5f, player.CurrentClipPoint);
+    }
+
+    private IntroloopAudio GetCurrentMusic(TensionLevel t)
+    {
+        _locationMusics = gameState.CurrentLocation.Music;
+        var newMusic = _defaultMusics[t];
+        if (_locationMusics.ContainsKey(t))
+        {
+            if (_locationMusics[t] != null)
+                newMusic = _locationMusics[t];
+        }
+        return newMusic;
     }
 }
