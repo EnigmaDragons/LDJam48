66674fda1830de36d1482c9ce9bc8d3e864c13f3
SilasReinagel <silas.reinagel@gmail.com>
2021-04-25 22:00:26 -0700

    Bug Fix: Doesn't punch the suspicion bar on start. Closes #274

--- src/LDJam48/Assets/CharacterSuspicion.cs
+++ src/LDJam48/Assets/CharacterSuspicion.cs
@@ -12,17 +12,18 @@ public class CharacterSuspicion : MonoBehaviour
     private void OnEnable()
     {
         character.ONSuspicionChange += UpdateUI;
-        UpdateUI();
+        UpdateUI(false);
     }
 
     private void OnDisable() => character.ONSuspicionChange -= UpdateUI;
 
-    private void UpdateUI()
+    private void UpdateUI() => UpdateUI(true);
+    private void UpdateUI(bool canPunch)
     {
         var currentSus = character.GetSuspicion();
         suspicionSlider.maxValue = character.GetMaxSuspicion();
         suspicionSlider.value = currentSus;
-        if (character.GetSuspicion() > 0 && currentSus > _lastValue)
+        if (canPunch && character.GetSuspicion() > 0 && currentSus > _lastValue)
             suspicionSlider.gameObject.transform.DOPunchScale(new Vector3(1.6f, 1.6f, 1.6f), 1f, 1);
         _lastValue = currentSus;
     }
